47f04f8ad9a38b24953aa130d2a88fd64096866e
Dhruti Shah <dhruti96shah@gmail.com>
2019-12-19 16:58:19 +0100

    Commenting clustering file

--- clustering_VCA.py
+++ clustering_VCA.py
@@ -8,6 +8,7 @@ from implementations import *
 from fcmeans import FCM
 from sklearn.cluster import KMeans
 
+#This function returns data after the dimension reduction using SVD
 def svd_decomposition( data ):
 	U, s, VT = np.linalg.svd(data)
 	S = np.diag(s[0:2])
@@ -15,11 +16,14 @@ def svd_decomposition( data ):
 	VT1 = VT[0:2,:]
 	return U1.dot(S.dot(VT1))
 
+#This function uses the given VCA function to implement vertex component analysis on the
+#  given hyperspectral data
 def VCA( data ):
 	Y = data.transpose()
 	Ae, indice, Yp = vca(Y,2)
 	plot( Ae.transpose(), 'VCA', 'Decomposition using VCA')
 
+# This function uses the f-cmeans library to perform fuzzy clustering of the data
 def fuzzy_clustering( data ):
 	svd = svd_decomposition( data )
 	fcm = FCM(n_clusters=2, m=1.2, max_iter=250)
@@ -28,6 +32,8 @@ def fuzzy_clustering( data ):
 	fcm_labels  = fcm.u.argmax(axis=1)
 	plot( fcm_centers, 'fuzzy', 'Decomposition using Fuzzy clustering')
 
+# This function uses the k-means given by sklearn to perfrom clustering on the data and
+# plot the centers
 def K_means( data ):
 	svd = svd_decomposition( data )
 	kmeans = KMeans(n_clusters=2, random_state=0).fit(svd)
